a7415b108b0c51d70e505d1b88cc412f5062d048
unknown <tayloreldonjohnson@gmail.com>
2017-08-15 16:30:14 -0700

    Angular

---
+++ OfficeStore.Web/wwwroot/NgApp/controllers/MainController.js
@@ -0,0 +1,23 @@
+﻿class MainController {
+	constructor($http) {
+		this.http = $http;
+		this.products = [];
+		this.getProducts();
+		this.product = {};
+	}
+
+	getProducts() {
+		this.http.get("api/Product")
+			.then(res => {
+				this.pets = res.data;
+			});
+	}
+
+	addProduct() {
+		this.http.post("api/Product", this.product)
+			.then(res => {
+				this.product = {};
+				this.getProducts();
+			});
+	}
+}
