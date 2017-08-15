5a204caa5de76d340ce22c77ea1b679aa69485fa
Rafib <rafiberger613@gmail.com>
2017-08-15 19:26:28 -0400

    added migrations

---
+++ OfficeStore.Data/Models/OfficeStoreContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace OfficeStore.Data.Models
+{
+    public class OfficeStoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=OfficeStore;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+        }
+    }
+}
---
+++ OfficeStore.Data/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeStore.Data.Models
+{
+   public  class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price{ get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
